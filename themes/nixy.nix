2a70223e1746b5fd72714d413952dfde91b012e4
shaunakpandit <shaunakdpandit@gmail.com>
2025-08-05 23:03:42 -0400

    update theme
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- themes/nixy.nix
+++ themes/nixy.nix
@@ -83,8 +83,8 @@
 
     polarity = "dark";
     image = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/black-and-white-forest_minimalist_black-and-white.png";
-      sha256 = "sha256-MOlLRQonZ6UAaSJlysjL8snxnMrSFH9VOLrjXaU82Kw=";
+      url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/moutains-and-stars_black.png";
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     };
   };
 }
