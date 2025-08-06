83170d4b5c4bf2bc99aa6705d8878db3e29b8c93
shaunakpandit <shaunakdpandit@gmail.com>
2025-08-06 19:27:38 -0400

    actually use tmux
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -1,4 +1,4 @@
 # Import all shell configurations
 {
-  imports = [./fzf.nix ./zsh.nix ./starship.nix ./zoxide.nix ./eza.nix];
+  imports = [./fzf.nix ./zsh.nix ./starship.nix ./zoxide.nix ./eza.nix ./tmux.nix];
 }
